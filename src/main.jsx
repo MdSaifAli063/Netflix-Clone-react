711be5caf7d8f32f141623d04fa58fdb6c727ff0
Mdsaif4363 <mdsaifali6303@gmail.com>
2025-11-30 00:20:59 +0530

    added

--- src/main.jsx
+++ src/main.jsx
@@ -4,7 +4,8 @@ import './index.css'
 import App from './App.jsx'
 import ReactDOM from 'react-dom';
 
-ReactDOM.render(
-	<App />,
-document.getElementById('app')
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+	<App />
+  </StrictMode>
 );
