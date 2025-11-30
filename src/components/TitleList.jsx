080ea3ad08bd69cc9c2fd0afdc1b83b0a5d2acf2
Mdsaif4363 <mdsaifali6303@gmail.com>
2025-12-01 00:10:04 +0530

    updated

--- src/components/TitleList.jsx
+++ src/components/TitleList.jsx
@@ -1,67 +1,55 @@
+import React, { useEffect, useState } from "react";
+import Item from "./Item";
 
+export default function TitleList({ url, title }) {
+  const apiKey = "87dfa1c669eea853da609d4968d294be";
+  const [data, setData] = useState([]);
+  const [mounted, setMounted] = useState(false);
 
-// Title List Container
+  const loadContent = async () => {
+    const requestUrl =
+      "https://api.themoviedb.org/3/" + url + "&api_key=" + apiKey;
 
-var TitleList = React.createClass({
-	apiKey: '87dfa1c669eea853da609d4968d294be',
-	getInitialState: function() {
-		return {data: [], mounted: false};
-	},
-	loadContent: function() {
-		var requestUrl = 'https://api.themoviedb.org/3/' + this.props.url + '&api_key=' + this.apiKey;
-		
-		$.ajax({
-      url: requestUrl,
-      dataType: 'json',
-      cache: false,
-      success: function(data) {
-        this.setState({data: data});
-				// console.log(data);
-      }.bind(this),
-      error: function(xhr, status, err) {
-        console.error(this.props.url, status, err.toString());
-      }.bind(this)
-    });
-	},
-	componentDidMount: function() {
-		this.loadContent();
-		this.setState({ mounted: true });
-	},
-	componentWillUnmount: function() {
-		this.setState({ mounted: false });
-	},
-	render: function() {
-		if(this.state.data.results) {
-			var titles = this.state.data.results.map(function(title, i) {
-				if(i < 5) {
+    try {
+      const res = await fetch(requestUrl);
+      const json = await res.json();
+      setData(json);
+    } catch (err) {
+      console.error("Failed to load list:", err);
+    }
+  };
 
-					var backDrop = 'http://image.tmdb.org/t/p/original' + title.backdrop_path;
-					if(!title.name) {
-						var name = title.original_title;
-					} else {
-						var name = title.name;
-					}
+  useEffect(() => {
+    loadContent();
+    setMounted(true);
+    return () => setMounted(false);
+  }, [url]);
 
-					return (
-						<Item key={title.id} title={name} score={title.vote_average} overview={title.overview} backdrop={backDrop} />
-					);	
+  const titles = data.results
+    ? data.results.slice(0, 5).map((title) => {
+        const backDrop =
+          "http://image.tmdb.org/t/p/original" + title.backdrop_path;
 
-				}
-			});	
+        const name = title.name || title.original_title;
 
-		} else {
-			var titles = '';
-		}
-		
-		return (
-			<div ref="titlecategory" className="TitleList" data-loaded={this.state.mounted}>
-				<div className="Title">
-					<h1>{this.props.title}</h1>
-					<div className="titles-wrapper">
-						{titles}
-					</div>
-				</div>
-			</div>
-		);
-	}
-});
+        return (
+          <Item
+            key={title.id}
+            title={name}
+            score={title.vote_average}
+            overview={title.overview}
+            backdrop={backDrop}
+          />
+        );
+      })
+    : "";
+
+  return (
+    <div className="TitleList" data-loaded={mounted}>
+      <div className="Title">
+        <h1>{title}</h1>
+        <div className="titles-wrapper">{titles}</div>
+      </div>
+    </div>
+  );
+}
